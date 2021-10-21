39a1dafd0e7c4265a4a298454bdaa8f004240144
Jem <chrisvin1997@gmail.com>
2021-10-22 04:41:42 +0530

    Set num of cols based on window width

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { Text, StyleSheet, FlatList, View, Image } from 'react-native';
+import { Text, StyleSheet, FlatList, View, Image, Dimensions } from 'react-native';
 import { Card } from 'react-native-elements'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import IngredientFlatList from "./IngredientFlatList";
+
+const windowWidth = Dimensions.get('window').width;
 
 function IngredientScreen() {
   return (
@@ -18,10 +20,10 @@ function IngredientScreen() {
           {name: 'Lorem', img_url: 'https://picsum.photos/204/200', description: 'Fusce eget mi eget nibh posuere lacinia sit amet sit amet metus. Vestibulum lacinia varius turpis, in gravida diam rhoncus id. Ut.'},
           {name: 'Ipsum', img_url: 'https://picsum.photos/204/201', description: 'Quisque dictum in ligula in lacinia. Donec bibendum varius quam, eu malesuada eros ultricies sed. Morbi ac velit ultrices, finibus quam eu.'},
         ]}
-        numColumns = {2}
+        numColumns = {windowWidth > 600 ? (windowWidth > 900 ? 3 : 2) : 1}
         renderItem = {
           ({item}) => 
-          <View style={{width:"50%"}}>
+          <View style={{width: windowWidth > 600 ? (windowWidth > 900 ? "33%" : "50%") : "100%"}}>
             <Card style={{flex: 1}}>
               <Card.Title>{item.name}</Card.Title>
               <Card.Divider/>
