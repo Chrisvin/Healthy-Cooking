c4b826ab660d0a3ef06c990a78a7fcaf5e8bc511
Jem <chrisvin1997@gmail.com>
2021-10-23 19:41:29 +0530

    Add pagination in ingredient list

--- Screens/IngredientSreen.js
+++ Screens/IngredientSreen.js
@@ -77,6 +77,9 @@ export function IngredientScreen() {
               </Card>
             </View>
           }
+          onEndReached = {(distanceFromEnd) =>
+            getIngredients(searchText, numberOfItems, offset, responseHandler)
+          }
         />
       </View>
   );
