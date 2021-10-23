6ed0797d782e77ad7a3f8a5911461238d8fc4e7b
Jem <chrisvin1997@gmail.com>
2021-10-23 19:20:58 +0530

    Move ingredient fetch fn to separate API file

---
+++ API/IngredientAPI.js
@@ -0,0 +1,12 @@
+import { SPOONACULAR_API_KEY } from '../Constants';
+
+export function getIngredients(query, number, offset, responseHandler) {
+    fetch(`https://api.spoonacular.com/food/ingredients/search?apiKey=${SPOONACULAR_API_KEY}&query=${query.length>0 ? query : 'a'}&number=${number}&offset=${offset}&metaInformation=true`)
+        .then(response => response.json())
+        .then(responseJson => {
+            responseHandler(responseJson);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
