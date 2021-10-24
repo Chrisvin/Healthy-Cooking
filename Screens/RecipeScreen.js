4d651fc34c7d60a1ba6832e3069d2a4c50e95a11
Jem <chrisvin1997@gmail.com>
2021-10-24 15:31:05 +0530

    Add search timer for recipescreen & optimize flatlist

--- Screens/RecipeScreen.js
+++ Screens/RecipeScreen.js
@@ -7,6 +7,7 @@ import { getRecipes } from '../API/RecipeAPI';
 const windowWidth = Dimensions.get('window').width;
 const numberOfItems = 15;
 const recipes = [];
+let searchTimer = setTimeout(()=>{}, 0);
 
 export function RecipeScreen({ navigation }) {
   const [searchText, setSearchText] = useState("");
@@ -39,13 +40,32 @@ export function RecipeScreen({ navigation }) {
   
   const updateSearch = (text) => {
     setSearchText(text);
-    getRecipes(text, numberOfItems, offset, responseHandler);
+    clearTimeout(searchTimer);
+    searchTimer = setTimeout(() => {
+      setOffet(0);
+      getRecipes(text, numberOfItems, 0, responseHandler);
+    }, 1000);
   };
 
   useEffect(() => {
     getRecipes(searchText, numberOfItems, offset, responseHandler);
   }, []);
 
+  const renderItem = ({item}) => (({item}) => 
+    <View style={{width: windowWidth > 600 ? (windowWidth > 900 ? "33%" : "50%") : "100%"}}>
+      <TouchableOpacity onPress={() => navigation.navigate('RecipeDetailsScreen', {itemID: item.id})}> 
+        <Card style={{flex: 1}}>
+            <View style={{flex:1, flexDirection: "row"}}>
+            <Image style={{flex:3, resizeMode:"contain"}} source={{uri: item.img_url}}/>
+            <View style={{flex:3, flexDirection: "column"}}>
+                <Card.Title style={{flex:1}}>{item.name}</Card.Title>
+                <Text style={{flex:2, paddingHorizontal:8}} numberOfLines={4} ellipsizeMod={'tail'}>{item.recipe_information}</Text>
+            </View>
+            </View>
+        </Card>
+      </TouchableOpacity>
+    </View>
+  );
   return (
       <View style={{flex:1}}>
         <SearchBar
@@ -56,24 +76,7 @@ export function RecipeScreen({ navigation }) {
           data={recipeData}
           numColumns = {windowWidth > 600 ? (windowWidth > 900 ? 3 : 2) : 1}
           keyExtractor = {item => item.id}
-          renderItem = {
-            ({item}) => 
-            <TouchableOpacity
-              style={{width: windowWidth > 600 ? (windowWidth > 900 ? "33%" : "50%") : "100%"}}
-              onPress={() => navigation.navigate('RecipeDetailsScreen', {itemID: item.id})}> 
-              <View>
-                <Card style={{flex: 1}}>
-                    <View style={{flex:1, flexDirection: "row"}}>
-                    <Image style={{flex:3, resizeMode:"contain"}} source={{uri: item.img_url}}/>
-                    <View style={{flex:3, flexDirection: "column"}}>
-                        <Card.Title style={{flex:1}}>{item.name}</Card.Title>
-                        <Text style={{flex:2, paddingHorizontal:8}} numberOfLines={4} ellipsizeMod={'tail'}>{item.recipe_information}</Text>
-                    </View>
-                    </View>
-                </Card>
-                </View>
-            </TouchableOpacity>
-          }
+          renderItem = { renderItem }
           onEndReached = {(distanceFromEnd) =>
             getRecipes(searchText, numberOfItems, offset, responseHandler)
           }
