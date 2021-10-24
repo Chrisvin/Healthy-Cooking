26b1ee21a37e7fabe32dc951248a9e082c317b0d
2020mt12033 <2020mt12033@wilp.bits-pilani.ac.in>
2021-10-24 17:47:01 +0530

    Add APIs to fetch recipe & ingredient info

--- API/IngredientAPI.js
+++ API/IngredientAPI.js
@@ -1,3 +1,7 @@
+/**
+ * Made by Chrisvin Jem Peniel - 2020mt12033@wilp.bits-pilani.ac.in
+ */
+
 import { SPOONACULAR_API_KEY } from '../Constants';
 
 export function getIngredients(query, number, offset, responseHandler) {
@@ -5,6 +9,17 @@ export function getIngredients(query, number, offset, responseHandler) {
         .then(response => response.json())
         .then(responseJson => {
             responseHandler(responseJson, query);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+export function getIngredientInformation(id, responseHandler) {
+    fetch(`https://api.spoonacular.com/food/ingredients/${id}/information?apiKey=${SPOONACULAR_API_KEY}`)
+        .then(response => response.json())
+        .then(responseJson => {
+            responseHandler(responseJson);
         })
         .catch((error) => {
             console.error(error);
--- API/RecipeAPI.js
+++ API/RecipeAPI.js
@@ -1,3 +1,7 @@
+/**
+ * Made by Chrisvin Jem Peniel - 2020mt12033@wilp.bits-pilani.ac.in
+ */
+
 import { SPOONACULAR_API_KEY } from '../Constants';
 
 export function getRecipes(query, number, offset, responseHandler) {
@@ -5,6 +9,17 @@ export function getRecipes(query, number, offset, responseHandler) {
         .then(response => response.json())
         .then(responseJson => {
             responseHandler(responseJson, query);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+export function getRecipeInformation(id, includeNutrition, responseHandler) {
+    fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${SPOONACULAR_API_KEY}&includeNutrition=${includeNutrition}`)
+        .then(response => response.json())
+        .then(responseJson => {
+            responseHandler(responseJson);
         })
         .catch((error) => {
             console.error(error);
