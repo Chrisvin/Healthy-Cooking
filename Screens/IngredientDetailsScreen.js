339c8c3e386697a102e312d08a70dcb27e9ae7e7
2020mt12033 <2020mt12033@wilp.bits-pilani.ac.in>
2021-10-24 17:47:45 +0530

    Implement skeleton JSON design for detail screens
    > Needs to implemented further by Kush & Vinod

--- Screens/IngredientDetailsScreen.js
+++ Screens/IngredientDetailsScreen.js
@@ -1,9 +1,23 @@
 import * as React from 'react';
+import  { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { getIngredientInformation } from '../API/IngredientAPI';
 
 export default function IngredientDetailsScreen({ route, navigation }) {
     const { itemID } = route.params;
+    const [ingredientDetails, setIngredientDetails] = useState("Loading");
+
+    const responseHandler = (responseJson) => {
+        console.log(responseJson);
+        setIngredientDetails(JSON.stringify(responseJson, null, 2));
+    }
+
+    useEffect(() => {
+        getIngredientInformation(itemID, responseHandler);
+    }, []);
+    
     return <View>
-        <Text>Details about the ingredient with id: ${itemID}</Text>
+        <Text style={{fontWeight:"bold", padding:12}}>Details about the ingredient with id: ${itemID}</Text>
+        <Text style={{paddingHorizontal:12}}><pre>Details about the ingredient with id: ${ingredientDetails}</pre></Text>
     </View>
 }
--- Screens/RecipeDetailsScreen.js
+++ Screens/RecipeDetailsScreen.js
@@ -1,9 +1,23 @@
 import * as React from 'react';
+import  { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { getRecipeInformation } from '../API/RecipeAPI';
 
 export default function RecipeDetailsScreen({ route, navigation }) {
     const { itemID } = route.params;
+    const [recipeDetails, setRecipeDetails] = useState("Loading");
+
+    const responseHandler = (responseJson) => {
+        console.log(responseJson);
+        setRecipeDetails(JSON.stringify(responseJson, null, 2));
+    }
+
+    useEffect(() => {
+        getRecipeInformation(itemID, false, responseHandler);
+    }, []);
+    
     return <View>
-        <Text>Details about the recipe with id: ${itemID}</Text>
+        <Text style={{fontWeight:"bold", padding:12}}>Details about the recipe with id: ${itemID}</Text>
+        <Text style={{paddingHorizontal:12}}><pre>Details about the recipe with id: ${recipeDetails}</pre></Text>
     </View>
 }
